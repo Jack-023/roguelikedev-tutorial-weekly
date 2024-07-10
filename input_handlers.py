6b9d5dbcee54669cbb96fff0f98f79f0028b6110
Jack Jordan <jackcjordan03@gmail.com>
2024-07-10 15:58:10 +1000

    Complete chapter #1 of the tutorial

---
+++ input_handlers.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+import tcod.event
+
+from actions import Action, EscapeAction, MovementAction
+
+
+class EventHandler(tcod.event.EventDispatch[Action]):
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = None
+
+        key = event.sym
+
+        if key == tcod.event.K_UP or key == tcod.event.KeySym.k:
+            action = MovementAction(dx=0, dy=-1)
+        elif key == tcod.event.K_DOWN or key == tcod.event.KeySym.j:
+            action = MovementAction(dx=0, dy=1)
+        elif key == tcod.event.K_LEFT or key == tcod.event.KeySym.h:
+            action = MovementAction(dx=-1, dy=0)
+        elif key == tcod.event.K_RIGHT or key == tcod.event.KeySym.l:
+            action = MovementAction(dx=1, dy=0)
+        elif key == tcod.event.KeySym.y:
+            action = MovementAction(dx=-1, dy=-1)
+        elif key == tcod.event.KeySym.u:
+            action = MovementAction(dx=1, dy=-1)
+        elif key == tcod.event.KeySym.b:
+            action = MovementAction(dx=-1, dy=1)
+        elif key == tcod.event.KeySym.n:
+            action = MovementAction(dx=1, dy=1)
+
+        elif key == tcod.event.K_ESCAPE:
+            action = EscapeAction()
+
+        # No valid key was pressed
+        return action

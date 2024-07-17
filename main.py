302d823143910835bd096a83caed56cd53881eaf
Jack Jordan <jackcjordan03@gmail.com>
2024-07-17 15:40:36 +1000

    Fix screen size, bump min tcod version.
    The window was rendering way too small at my native desktop resolution
    (5760x3240). It now defaults to 2560x1440. This is probably too large
    for a lot of contexts. The window will scale properly if you manually
    resize it. Otherwise just change the values for screen_width and
    screen_height in main.py.

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
 
+screen_width = 2160
+screen_height = 1440
+
+map_width = 80
+map_height = 45
+
 
 def main():
-    screen_width = 80
-    screen_height = 50
-
-    map_width = 80
-    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -20,25 +21,25 @@ def main():
 
     event_handler = EventHandler()
 
-    player = Entity(int(screen_width / 2),
-                    int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5),
-                 int(screen_height / 2), "@", (255, 255, 0))
+    player = Entity(int(map_width / 2),
+                    int(map_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(map_width / 2 - 5),
+                 int(map_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
     game_map = GameMap(map_width, map_height)
 
     engine = Engine(entities, event_handler, game_map, player)
 
-    with tcod.context.new_terminal(
-        screen_width,
-        screen_height,
+    with tcod.context.new(
+        width=screen_width,
+        height=screen_height,
         tileset=tileset,
         title="Yet Another Roguelike Tutorial",
         vsync=True,
     ) as context:
         root_console = tcod.console.Console(
-            screen_width, screen_height, order="F")
+            map_width, map_height, order="F")
         while True:
             engine.render(root_console, context)
 
